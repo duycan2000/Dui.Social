1f9a0d883c8e0f313ed6f69d1f481be25de566b2
duycan2000 <87920785+duycan2000@users.noreply.github.com>
2026-04-06 11:51:42 +0700

    1

---
+++ cript.js
@@ -0,0 +1,67 @@
+tailwind.config = {
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        "on-secondary-fixed": "#241a00",
+        "background": "#131313",
+        "on-secondary-container": "#342800",
+        "inverse-primary": "#805600",
+        "surface-container-low": "#1c1b1b",
+        "primary": "#ffe1b8",
+        "surface": "#131313",
+        "secondary-fixed": "#ffe088",
+        "secondary-container": "#af8d11",
+        "tertiary": "#ffdfd6",
+        "surface-container-high": "#2a2a2a",
+        "on-surface": "#e5e2e1",
+        "on-tertiary-fixed": "#3b0900",
+        "surface-variant": "#353534",
+        "tertiary-fixed-dim": "#ffb5a0",
+        "on-primary-fixed": "#281800",
+        "outline": "#9c8f78",
+        "surface-dim": "#131313",
+        "on-tertiary": "#601400",
+        "on-secondary-fixed-variant": "#574500",
+        "error-container": "#93000a",
+        "primary-fixed": "#ffddaf",
+        "on-secondary": "#3c2f00",
+        "on-tertiary-fixed-variant": "#872000",
+        "on-primary-fixed-variant": "#614000",
+        "error": "#ffb4ab",
+        "surface-container-highest": "#353534",
+        "on-error": "#690005",
+        "on-primary-container": "#734d00",
+        "on-tertiary-container": "#a02800",
+        "secondary-fixed-dim": "#e9c349",
+        "inverse-on-surface": "#313030",
+        "surface-bright": "#393939",
+        "surface-container": "#201f1f",
+        "tertiary-container": "#ffb9a6",
+        "surface-tint": "#ffba43",
+        "outline-variant": "#504532",
+        "on-primary": "#432c00",
+        "primary-container": "#ffbe51",
+        "surface-container-lowest": "#0e0e0e",
+        "tertiary-fixed": "#ffdbd1",
+        "on-surface-variant": "#d4c5ab",
+        "inverse-surface": "#e5e2e1",
+        "on-background": "#e5e2e1",
+        "secondary": "#e9c349",
+        "primary-fixed-dim": "#ffba43",
+        "on-error-container": "#ffdad6"
+      },
+      borderRadius: {
+        "DEFAULT": "0.125rem",
+        "lg": "0.25rem",
+        "xl": "0.5rem",
+        "full": "0.75rem"
+      },
+      fontFamily: {
+        "headline": ["Epilogue"],
+        "body": ["Manrope"],
+        "label": ["Manrope"]
+      }
+    }
+  }
+};
